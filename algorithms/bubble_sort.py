b2e2f22ae026cd161b7cff1f55e5aafe84a846ca
eraberisha <era.berisha5@student.uni-pr.edu>
2025-12-02 09:41:26 +0100

    Implementation of Bubble Sort

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -6,4 +6,23 @@ def bubble_sort(arr):
     Returns:
         tuple: (sorted_list, comparisons, swaps)
     """
-    pass
+    n=len(arr)
+    comparisons= 0
+    swaps= 0
+    arr= arr.copy()
+
+    for i in range(n):
+        swapped= False;
+
+        for j in range(0,n-i-1):
+            comparisons+=1
+
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swaps+=1
+                swapped=True
+
+        if not swapped:
+            break
+
+    return arr,comparisons,swaps
