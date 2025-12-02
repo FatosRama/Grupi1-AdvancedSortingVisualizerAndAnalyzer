ecb0f3e04567dce9ec9422945a3089adaebefa9c
Fatos Rama <fatosrama.fr@gmail.com>
2025-12-02 22:09:14 +0100

    Added finalize(), register(), increment(), finish() and getRanking() functions to the stats collector

--- utils/stats_collector.py
+++ utils/stats_collector.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Dict
+import time
 
 @dataclass
 class SortStats:
@@ -14,3 +15,30 @@ class SortStats:
 class StatsCollector:
     def __init__(self):
         self.stats: Dict[str, SortStats] = {}
+
+
+@property
+def elapsed(self) -> float:
+    return self.end_time - self.start_time if self.finished else time.time() - self.start_time
+
+def finalize(self):
+    self.end_time = time.time()
+    self.finished = True
+
+def register(self, algo_name: str):
+    self.stats[algo_name] = SortStats(name=algo_name, start_time=time.time())
+
+def increment(self, algo_name: str, comparisons=0, swaps=0, accesses=0):
+    s = self.stats.get(algo_name)
+    if s:
+        s.comparisons += comparisons
+        s.swaps += swaps
+        s.array_accesses += accesses
+
+def finish(self, algo_name: str):
+    if algo_name in self.stats:
+        self.stats[algo_name].finalize()
+
+def get_ranking(self):
+    finished = [s for s in self.stats.values() if s.finished]
+    return sorted(finished, key=lambda x: x.elapsed)
