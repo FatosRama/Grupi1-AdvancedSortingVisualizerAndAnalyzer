cefb97105d92e2f859528acd51bb5112f27f5d8e
Fatos Rama <fatosrama.fr@gmail.com>
2025-12-02 22:12:15 +0100

    Finished the stats collector by adding the getAll function

--- utils/stats_collector.py
+++ utils/stats_collector.py
@@ -12,33 +12,35 @@ class SortStats:
     end_time: float = 0.0
     finished: bool = False
 
+    @property
+    def elapsed(self) -> float:
+        return self.end_time - self.start_time if self.finished else time.time() - self.start_time
+
+    def finalize(self):
+        self.end_time = time.time()
+        self.finished = True
+
 class StatsCollector:
     def __init__(self):
         self.stats: Dict[str, SortStats] = {}
 
+    def register(self, algo_name: str):
+        self.stats[algo_name] = SortStats(name=algo_name, start_time=time.time())
 
-@property
-def elapsed(self) -> float:
-    return self.end_time - self.start_time if self.finished else time.time() - self.start_time
+    def increment(self, algo_name: str, comparisons=0, swaps=0, accesses=0):
+        s = self.stats.get(algo_name)
+        if s:
+            s.comparisons += comparisons
+            s.swaps += swaps
+            s.array_accesses += accesses
 
-def finalize(self):
-    self.end_time = time.time()
-    self.finished = True
+    def finish(self, algo_name: str):
+        if algo_name in self.stats:
+            self.stats[algo_name].finalize()
 
-def register(self, algo_name: str):
-    self.stats[algo_name] = SortStats(name=algo_name, start_time=time.time())
-
-def increment(self, algo_name: str, comparisons=0, swaps=0, accesses=0):
-    s = self.stats.get(algo_name)
-    if s:
-        s.comparisons += comparisons
-        s.swaps += swaps
-        s.array_accesses += accesses
-
-def finish(self, algo_name: str):
-    if algo_name in self.stats:
-        self.stats[algo_name].finalize()
-
-def get_ranking(self):
-    finished = [s for s in self.stats.values() if s.finished]
-    return sorted(finished, key=lambda x: x.elapsed)
+    def get_ranking(self):
+        finished = [s for s in self.stats.values() if s.finished]
+        return sorted(finished, key=lambda x: x.elapsed)
+    
+    def get_all(self):
+        return list(self.stats.values())
