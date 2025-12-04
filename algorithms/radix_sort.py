cae8baf3e4d675835e66c22a9aa5b06a311f9a15
eraberisha <era.berisha5@student.uni-pr.edu>
2025-12-04 18:16:56 +0100

    Built output array, copy back, and finalized with visualization

--- algorithms/radix_sort.py
+++ algorithms/radix_sort.py
@@ -11,7 +11,9 @@ def radix_sort(vis):
     while max_num // exp>0:
         counting_sort_radix(vis,exp)
         exp *=10
+
     vis.mark_sorted()
+    yield
 
 def counting_sort_radix(vis, exp):
         arr = vis.arr
@@ -31,3 +33,21 @@ def counting_sort_radix(vis, exp):
             count[i] += count[i - 1]
             vis.steps += 1
             yield
+
+        for i in range(n - 1, -1, -1):
+            index = (arr[i] // exp) % 10
+            output_idx = count[index] - 1
+            output[output_idx] = arr[i]
+            count[index] -= 1
+            vis.steps += 1
+            if hasattr(vis, 'update_display'):
+                vis.update_display([i], f"Placing based on digit {index}")
+            yield
+
+        for i in range(n):
+            if arr[i] != output[i]:
+                arr[i] = output[i]
+                vis.steps += 1
+                if hasattr(vis, 'update_display'):
+                    vis.update_display([i], f"Updating position {i}")
+                yield
