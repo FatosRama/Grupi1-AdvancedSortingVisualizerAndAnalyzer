474fd5c198500c1f2d348023b7ef2905802ab6f9
Erblin Syla <erblin.sylaa@gmail.com>
2025-12-04 20:28:40 +0100

    Implementation of pure selection sort algorithm

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,2 +1,12 @@
 def selection_sort(vis):
-    return
+    arr = vis.arr
+    n = len(arr)
+
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
