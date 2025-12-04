94628aba2cf72af98b3d6d8c91132b749b5fd838
eraberisha <era.berisha5@student.uni-pr.edu>
2025-12-04 13:04:53 +0100

    Implemented counting and prefix sum with yield for visualization

--- algorithms/radix_sort.py
+++ algorithms/radix_sort.py
@@ -9,7 +9,25 @@ def radix_sort(vis):
     exp=1
 
     while max_num // exp>0:
-        #counting_sort_radix(vis,exp)
+        counting_sort_radix(vis,exp)
         exp *=10
-
     vis.mark_sorted()
+
+def counting_sort_radix(vis, exp):
+        arr = vis.arr
+        n = len(arr)
+        output = [0] * n
+        count = [0] * 10
+
+        for i in range(n):
+            index = (arr[i] // exp) % 10
+            count[index] += 1
+            vis.steps += 1
+            if hasattr(vis, 'update_display'):
+                vis.update_display([i], f"Digit {index} (exp={exp})")
+            yield
+            
+        for i in range(1, 10):
+            count[i] += count[i - 1]
+            vis.steps += 1
+            yield
