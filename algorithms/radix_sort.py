b42304ae8102bf98b17e6c5e68e4fc28437d2d62
eraberisha <era.berisha5@student.uni-pr.edu>
2025-12-04 10:20:09 +0100

    Added initial skeleton for Radix Sort function

--- algorithms/radix_sort.py
+++ algorithms/radix_sort.py
@@ -1,62 +1,15 @@
-def radix_sort(arr):
-    """
-    Radix Sort Algorithm (base 10).
-    Params:
-        arr (list[int]): Input list of integers.
-    Returns:
-        tuple: (sorted_list, operations count)
-    Note:
-        Radix Sort is stable and non-compartive.
-        Track operations, not comparisons.
-    """
+def radix_sort(vis):
+    arr=vis.arr
     if not arr:
-        return [],0
+        vis.mark_sorted()
+        yield
+        return
 
-    arr= arr.copy()
-    operations= 0
+    max_num=max(arr)
+    exp=1
 
-    negatives=[x for x in arr if x<0]
-    non_negatives=[x for x in arr if x>=0]
+    while max_num // exp>0:
+        #counting_sort_radix(vis,exp)
+        exp *=10
 
-    def counting_sort_by_digit(array, exp):
-        nonlocal operations
-        output= [0] * len(array)
-        count= [0] *10
-
-        for num in array:
-            digit=(num // exp)%10
-            count[digit] +=1
-            operations +=1
-
-        for i in range(1,10):
-            count[i]+=count[i-1]
-            operations +=1
-
-        for num in reversed(array):
-            digit=(num // exp)%10
-            count[digit]-=1
-            output[count[digit]]=num
-            operations+=1
-
-        return output
-
-    if non_negatives:
-        max_val=max(non_negatives)
-        exp=1
-        while max_val // exp>0:
-            non_negatives=counting_sort_by_digit(non_negatives,exp)
-            exp*=10
-            operations+=1
-
-    if negatives:
-        negatives=[-x for x in negatives]
-        max_val=max(negatives)
-        exp=1
-        while max_val // exp>0:
-            negatives= counting_sort_by_digit(negatives,exp)
-            exp*=10
-            operations+=1
-        negatives=[-x for x in reversed(negatives)]
-
-    sorted_arr=negatives+non_negatives
-    return sorted_arr, operations
+    vis.mark_sorted()
