c69af36cb9ac9ca84c670a40688fcb2b40b33eca
Ereza Greicevci <ereza.greicevci@student.uni-pr.edu>
2025-12-05 23:33:49 +0100

    Deleted previous Bucket Sort version and added initial setup for bucket_sort()

--- algorithms/bucket_sort.py
+++ algorithms/bucket_sort.py
@@ -1,38 +1,6 @@
-def bucket_sort(arr):
+def bucket_sort(vis):
+    arr = vis.arr
     if not arr:
-        yield arr[:], 0, 0, ()
+        vis.mark_sorted()
+        yield
         return
-
-    arr = arr.copy()
-    n = len(arr)
-    comparisons = writes = 0
-
-    min_val, max_val = min(arr), max(arr)
-    if max_val == min_val:
-        yield arr[:], comparisons, writes, ()
-        return
-
-    buckets = [[] for _ in range(n)]
-
-    for i, num in enumerate(arr):
-        bucket_idx = int((num - min_val) / (max_val - min_val) * (n - 1))
-        buckets[bucket_idx].append((i, num))
-        yield arr[:], comparisons, writes, (i,)
-
-    pos = 0
-    for bucket in buckets:
-        for i in range(1, len(bucket)):
-            key_idx, key = bucket[i]
-            j = i - 1
-            while j >= 0 and bucket[j][1] > key:
-                comparisons += 1
-                bucket[j + 1] = bucket[j]
-                j -= 1
-            bucket[j + 1] = (key_idx, key)
-        for orig_idx, value in bucket:
-            arr[pos] = value
-            writes += 1
-            yield arr[:], comparisons, writes, (pos,)
-            pos += 1
-
-    yield arr[:], comparisons, writes, ()
