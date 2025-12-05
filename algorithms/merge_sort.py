92816beeb6218337b329ae1dc3313983595b51ca
Ereza Greicevci <ereza.greicevci@student.uni-pr.edu>
2025-12-05 23:22:47 +0100

    Implemented merge function with visualization steps

--- algorithms/merge_sort.py
+++ algorithms/merge_sort.py
@@ -13,3 +13,34 @@ def merge_sort(vis, left=0, right=None):
     if left == 0 and right == len(vis.arr) - 1:
         vis.mark_sorted()
         yield
+
+        def merge(vis, left, mid, right):
+            arr = vis.arr
+            left_part = arr[left:mid + 1]
+            right_part = arr[mid + 1:right + 1]
+
+            i = j = 0
+            k = left
+
+            while i < len(left_part) and j < len(right_part):
+                vis.compare(left + i, mid + 1 + j)
+                yield
+
+                if left_part[i] <= right_part[j]:
+                    if arr[k] != left_part[i]:
+                        arr[k] = left_part[i]
+                        # Visualize the copy
+                        vis.steps += 1
+                        if hasattr(vis, 'update_display'):
+                            vis.update_display([k], "Merging")
+                        yield
+                    i += 1
+                else:
+                    if arr[k] != right_part[j]:
+                        arr[k] = right_part[j]
+                        vis.steps += 1
+                        if hasattr(vis, 'update_display'):
+                            vis.update_display([k], "Merging")
+                        yield
+                    j += 1
+                k += 1
