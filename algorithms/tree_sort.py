1961c71eb0ed91915bfdc700f092d4c5d9b58a91
eraberisha <era.berisha5@student.uni-pr.edu>
2025-12-05 23:32:49 +0100

    Started tree_sort function and added empty array handling

--- algorithms/tree_sort.py
+++ algorithms/tree_sort.py
@@ -3,3 +3,10 @@ class TreeNode:
         self.val = val
         self.left = None
         self.right = None
+
+    def tree_sort(vis):
+        arr = vis.arr
+        if not arr:
+            vis.mark_sorted()
+            yield
+            return
