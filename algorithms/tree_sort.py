2813400285615d4cce6f69a34b89e231450e4c68
eraberisha <era.berisha5@student.uni-pr.edu>
2025-12-06 20:26:56 +0100

    Added insert function

--- algorithms/tree_sort.py
+++ algorithms/tree_sort.py
@@ -4,9 +4,18 @@ class TreeNode:
         self.left = None
         self.right = None
 
-    def tree_sort(vis):
+def tree_sort(vis):
         arr = vis.arr
         if not arr:
             vis.mark_sorted()
             yield
             return
+
+def insert(root,val):
+        if not root:
+            return TreeNode(val)
+
+        if val<root.val:
+            root.left=insert(root.left, val)
+        else:
+            root.right=insert(root.right, val)
