e9bee0136adad66546587acaba938d167aff2444
Erblin Syla <erblin.sylaa@gmail.com>
2025-12-06 21:48:16 +0100

    Adding the partitioning logic to quick sort

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -2,3 +2,26 @@ def quick_sort(vis):
     arr = vis.arr
     n = len(arr)
     stack = [(0, n - 1)]
+
+    while stack:
+        low, high = stack.pop()
+
+        if low < high:
+            pivot = arr[high]
+            i = low - 1
+
+            for j in range(low, high):
+                vis.compare(j, high)
+                yield
+
+                if arr[j] <= pivot:
+                    i += 1
+                    if i != j:
+                        vis.swap(i, j)
+                        yield
+
+            if i + 1 != high:
+                vis.swap(i + 1, high)
+                yield
+
+            pivot_index = i + 1
