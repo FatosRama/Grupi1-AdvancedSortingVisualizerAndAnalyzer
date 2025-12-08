4d9787d94a0eda7a281259ec49ea2641136c4115
eraberisha <era.berisha5@student.uni-pr.edu>
2025-12-08 16:41:47 +0100

    Implemented counting frequency loop and added visualization

--- algorithms/counting_sort.py
+++ algorithms/counting_sort.py
@@ -12,3 +12,12 @@ def counting_sort(vis):
 
     count=[0]*range_val
     output=[0]*len(arr)
+
+    for idx, num in enumerate(arr):
+        count[num-min_val]+=1
+        vis.steps +=1
+
+        if hasattr(vis, "update_display"):
+            vis.update_display([idx], f"Counting: {num}")
+
+        yield
