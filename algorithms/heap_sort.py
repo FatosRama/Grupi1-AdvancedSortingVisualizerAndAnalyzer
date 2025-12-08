7ea7728c5ac54fcca8d3e23e6cbdd89b7677e2e1
Ereza Greicevci <ereza.greicevci@student.uni-pr.edu>
2025-12-08 16:07:55 +0100

    Implemented max-heap build and extraction steps in heap_sort

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -1,3 +1,16 @@
 def heap_sort(vis):
     arr = vis.arr
     n = len(arr)
+
+    # Build max heap
+    for i in range(n // 2 - 1, -1, -1):
+        yield from heapify(vis, n, i)
+
+    # Extract elements
+    for i in range(n - 1, 0, -1):
+        vis.swap(0, i)
+        yield
+        yield from heapify(vis, i, 0)
+
+    vis.mark_sorted()
+    yield
