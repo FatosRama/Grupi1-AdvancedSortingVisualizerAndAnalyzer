f4534001c1c4a6d6bb255a3d4570ac58a507e9ea
eraberisha <era.berisha5@student.uni-pr.edu>
2025-12-08 16:38:47 +0100

    Added min/max calculation and initialized count/output arrays

--- algorithms/counting_sort.py
+++ algorithms/counting_sort.py
@@ -5,3 +5,10 @@ def counting_sort(vis):
         vis.mark_sorted()
         yield
         return
+
+    max_val=max(arr)
+    min_val=min(arr)
+    range_val=max_val-min_val+1
+
+    count=[0]*range_val
+    output=[0]*len(arr)
